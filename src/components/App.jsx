be616e8e12d449aa55df3de5ecb006d655526c6d
achytent <achytent@gmail.com>
2023-08-07 09:43:41 +0300

    Bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,36 @@
-import Game from './Game.jsx';
-import Header from './Header.jsx';
-import {useState} from "react";
-import '../styles/App.css';
+import Game from "./Game.jsx";
+import Header from "./Header.jsx";
+import { useState } from "react";
+import "../styles/App.css";
 
 export default function App() {
-    const [score, setScore] = useState(0);
-    const [bestScore, setBestScore] = useState(0);
-    const [usedElements, setUsedElements] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [usedElements, setUsedElements] = useState([]);
 
-    function handleAddUsedElement(event) {
-        if(usedElements.includes(event.target.textContent)){
-            console.log(usedElements);
-            console.log(event.target.textContent)
-            bestScore < score ? setBestScore(score) : setBestScore(bestScore);
-            setScore(0);
-            setUsedElements([]);
-            return;
-        }
-        setUsedElements((prevUsedElements) => [
-            ...prevUsedElements,
-            event.target.textContent
-        ])
-        setScore(score + 1);
+  function handleAddUsedElement(event) {
+    if (usedElements.includes(event.target.dataset.symbol)) {
+      console.log(usedElements);
+      console.log(event.target.dataset.symbol);
+      bestScore < score ? setBestScore(score) : setBestScore(bestScore);
+      setScore(0);
+      setUsedElements([]);
+      return;
     }
+    setUsedElements((prevUsedElements) => [
+      ...prevUsedElements,
+      event.target.dataset.symbol,
+    ]);
+    setScore(score + 1);
+  }
 
-    return(
-        <div className="page-body">
-            <Header
-                score={score}
-                bestScore={bestScore}
-            />
-            <Game
-                usedElements={usedElements}
-                handleAddUsedElement={handleAddUsedElement}
-            />
-        </div>
-    )
+  return (
+    <div className="page-body">
+      <Header score={score} bestScore={bestScore} />
+      <Game
+        usedElements={usedElements}
+        handleAddUsedElement={handleAddUsedElement}
+      />
+    </div>
+  );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,51 +1,97 @@
-import elements from '../assets/PeriodicTableJSON.json';
-import '../styles/Game.css';
+import elements from "../assets/PeriodicTableJSON.json";
+import "../styles/Game.css";
 
 export default function Game(props) {
-    const handleClick = props.handleAddUsedElement;
-    const usedElements = props.usedElements;
+  const handleClick = props.handleAddUsedElement;
+  const usedElements = props.usedElements;
 
-    const elementsToShow = [];
-    let repeatingElements = 0;
-    for(let i = 0; i < 6; i++) {
-        const newElement = elements.elements[Math.floor(Math.random()*100)];
-        if(usedElements.includes(newElement.symbol))
-            repeatingElements += 1;
-        if(repeatingElements === 6) {
-            i -= 1;
-            repeatingElements -= 1;
-        } else {
-            elementsToShow.push(newElement)
-        }
+  const elementsToShow = [];
+  let repeatingElements = 0;
+  for (let i = 0; i < 6; i++) {
+    const newElement = elements.elements[Math.floor(Math.random() * 100)];
+    if (usedElements.includes(newElement.symbol)) repeatingElements += 1;
+    if (repeatingElements === 6) {
+      i -= 1;
+      repeatingElements -= 1;
+    } else {
+      elementsToShow.push(newElement);
     }
+  }
 
-
-    return(
-        <div className="game">
-            <div className="card" onClick={handleClick}>
-                <div className='symbol'>{elementsToShow[0].symbol}</div>
-                <div className='element'>{elementsToShow[0].name}</div>
-            </div>
-            <div className="card" onClick={handleClick}>
-                <div className='symbol'>{elementsToShow[1].symbol}</div>
-                <div className='element'>{elementsToShow[1].name}</div>
-            </div>
-            <div className="card" onClick={handleClick}>
-                <div className='symbol'>{elementsToShow[2].symbol}</div>
-                <div className='element'>{elementsToShow[2].name}</div>
-            </div>
-            <div className="card" onClick={handleClick}>
-                <div className='symbol'>{elementsToShow[3].symbol}</div>
-                <div className='element'>{elementsToShow[3].name}</div>
-            </div>
-            <div className="card" onClick={handleClick}>
-                <div className='symbol'>{elementsToShow[4].symbol}</div>
-                <div className='element'>{elementsToShow[4].name}</div>
-            </div>
-            <div className="card" onClick={handleClick}>
-                <div className='symbol'>{elementsToShow[5].symbol}</div>
-                <div className='element'>{elementsToShow[5].name}</div>
-            </div>
+  return (
+    <div className="game">
+      <div
+        className="card"
+        onClick={handleClick}
+        data-symbol={elementsToShow[0].symbol}
+      >
+        <div className="symbol" data-symbol={elementsToShow[0].symbol}>
+          {elementsToShow[0].symbol}
         </div>
-    )
+        <div className="element" data-symbol={elementsToShow[0].symbol}>
+          {elementsToShow[0].name}
+        </div>
+      </div>
+      <div
+        className="card"
+        onClick={handleClick}
+        data-symbol={elementsToShow[1].symbol}
+      >
+        <div className="symbol" data-symbol={elementsToShow[1].symbol}>
+          {elementsToShow[1].symbol}
+        </div>
+        <div className="element" data-symbol={elementsToShow[1].symbol}>
+          {elementsToShow[1].name}
+        </div>
+      </div>
+      <div
+        className="card"
+        onClick={handleClick}
+        data-symbol={elementsToShow[2].symbol}
+      >
+        <div className="symbol" data-symbol={elementsToShow[2].symbol}>
+          {elementsToShow[2].symbol}
+        </div>
+        <div className="element" data-symbol={elementsToShow[2].symbol}>
+          {elementsToShow[2].name}
+        </div>
+      </div>
+      <div
+        className="card"
+        onClick={handleClick}
+        data-symbol={elementsToShow[3].symbol}
+      >
+        <div className="symbol" data-symbol={elementsToShow[3].symbol}>
+          {elementsToShow[3].symbol}
+        </div>
+        <div className="element" data-symbol={elementsToShow[3].symbol}>
+          {elementsToShow[3].name}
+        </div>
+      </div>
+      <div
+        className="card"
+        onClick={handleClick}
+        data-symbol={elementsToShow[4].symbol}
+      >
+        <div className="symbol" data-symbol={elementsToShow[4].symbol}>
+          {elementsToShow[4].symbol}
+        </div>
+        <div className="element" data-symbol={elementsToShow[4].symbol}>
+          {elementsToShow[4].name}
+        </div>
+      </div>
+      <div
+        className="card"
+        onClick={handleClick}
+        data-symbol={elementsToShow[5].symbol}
+      >
+        <div className="symbol" data-symbol={elementsToShow[5].symbol}>
+          {elementsToShow[5].symbol}
+        </div>
+        <div className="element" data-symbol={elementsToShow[5].symbol}>
+          {elementsToShow[5].name}
+        </div>
+      </div>
+    </div>
+  );
 }
